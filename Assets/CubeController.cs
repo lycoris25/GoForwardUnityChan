9e1af9fc4841a3f9f5dbf3c27fdece9dde285024
lycoris25 <lycoris8460>
2018-06-07 18:53:10 +0900

    GoForwardUnityChan Lesson7課題提出
    ブロックが落ちると音が鳴るようにしました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	private float speed = -0.2f;
+	private float deadLine = -10;
+
+	private GameObject unityChan;
+
+
+	// Use this for initialization
+	void Start () {
+		unityChan = GameObject.Find ("Unitychan2D");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate (this.speed, 0, 0);
+
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+
+		if (other.gameObject.tag == "CubeTag") {
+			GetComponent<AudioSource> ().Play ();
+		} else if(other.gameObject.tag == "GroundTag"){
+				GetComponent<AudioSource> ().Play ();
+			}
+	}
+}
